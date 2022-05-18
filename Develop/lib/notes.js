d7cfaf515e2c19d4fab0760d7ac0ff3218e75f98
BrandonDaws <velarde.brandon02@gmail.com>
2022-05-18 14:31:08 -0500

    WE HAD THE PATHS SETUP WRONG IM SO MAD YET SO HAPPY TO BE DONE WITH IT. MVP

--- Develop/lib/notes.js
+++
@@ -1,3 +0,0 @@
-const router = require('express').Router();
-const {notes} = require('../db/db.json');
-const {} = require('../public/assets/js/index');
--- Develop/routes/apiRoute/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const notesRoutes = require('../apiRoute/notes.js');
-
-router.use(notesRoutes);
-
-module.exports = router;
